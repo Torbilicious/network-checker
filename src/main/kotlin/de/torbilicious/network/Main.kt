624d21f8304c74e19e78cf22932f7c3d5c37ecde
Torbilicious <torben.binder@googlemail.com>
2017-11-20 16:21:39 +0100

    Enable checking ping to google facebook and lol servers

--- src/main/kotlin/de/torbilicious/network/Main.kt
+++ src/main/kotlin/de/torbilicious/network/Main.kt
@@ -1,5 +1,50 @@
 package de.torbilicious.network
 
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
+import org.icmp4j.IcmpPingRequest
+import org.icmp4j.IcmpPingUtil
+
+
 fun main(args: Array<String>) {
-    println("hi")
+    request()
+}
+
+fun request() {
+
+    val requests = mutableListOf<IcmpPingRequest>()
+    requests.addAll(createRequests("www.google.com", 10))
+    requests.addAll(createRequests("www.facebook.com", 10))
+    requests.addAll(createRequests("104.160.141.3", 10))
+
+    val deferred = requests.map {
+        async {
+            IcmpPingUtil.executePingRequest(it)
+        }
+    }
+
+
+    val responses = runBlocking {
+        deferred.map { it.await() }
+    }
+
+    responses.forEach {
+        val formattedResponse = "Host '${it.host}': ${it.duration}ms"
+        println(formattedResponse)
+    }
+
+    println("Average time: ${responses.map { it.duration }.average()}ms")
+}
+
+fun createRequests(host: String, amount: Int): Iterable<IcmpPingRequest> {
+    val requests = mutableListOf<IcmpPingRequest>()
+
+    for (count in 1..amount) {
+        val request = IcmpPingUtil.createIcmpPingRequest()
+        request.host = host
+
+        requests.add(request)
+    }
+
+    return requests
 }
